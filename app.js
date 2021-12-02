d83594816d91b870638813b7aa6da87bf62562d9
Carlos Ceballo <carlos96cacv@gmail.com>
2021-12-02 11:26:59 -0400

    Primera actualizacion

---
+++ app.js
@@ -0,0 +1,37 @@
+const container = document.getElementById('container')
+
+container.style.height = visualViewport.height + 'px';
+container.style.width = visualViewport.width + 'px';
+
+
+function validate(){
+
+    let mensaje = document.getElementById('mensaje');
+    let contenido = document.getElementById('contenido');
+    let text = document.getElementById('text');
+
+    if(mensaje.value == ""){
+        contenido.className = 'noneS'
+        contenido1.className = 'contenido1'
+        text.innerHTML = '<p>Hace un tiepo llegué a decir que eres como un agujero negro para mi, que me atraías con tanta fuerza que me era imposible escapar cuando lo dije no te conocía tanto como ahora, pero cuanta razon tenía, desde el momento en el que decidí decirte lo que sentía sabía que iba directo hacia ti y que no había vuelta atras, temía un miedo brutal a estrellarme pero eso no me detuvo y sin duda se volvió en una de mis mejores decisiones hasta ahora.</p>'
+    }
+}
+
+
+
+/*
+
+Hace un tiepo llegué a decir que eres como un agujero negro para mi, que me atraías con tanta fuerza que me era imposible escapar cuando lo dije no te conocía tanto como ahora, pero cuanta razon tenía, desde el momento en el que decidí decirte lo que sentía sabía que iba directo hacia ti y que no había vuelta atras, temía un miedo brutal a estrellarme pero eso no me detuvo y sin duda se volvió en una de mis mejores decisiones hasta ahora.
+
+Es raro que en unos cuantos meses lograste volcar mi vida por completo, todo estaba configurado para no enamorarme, para no sufrir, para no sentir y lo estaba logrando muy bien, hasta que llegaste e hiciste pedazos mi seguridad, y te colaste a lo profundo de mi alma y se que de ahí no podré sacarte jamás, ya mi alma y corazón y todo yo, es tuyo.
+
+
+
+
+
+For you, I'd bleed myself dry
+
+
+https://www.youtube.com/watch?v=sKx8AVS3Pu8
+
+*/
